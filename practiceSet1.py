d4696adcfe8212151da99201d223e63dc0fa9190
Pranali Waghmare <126958148+Pranali2624@users.noreply.github.com>
2024-05-01 16:19:28 +0530

    Create practiceSet1.py

---
+++ practiceSet1.py
@@ -0,0 +1,34 @@
+import os
+# first program of python 
+print("hello world")
+print ("5")
+
+# write a pome in python 
+print("Perfection is no attainable")
+print("but if we chase perfection we can catch excellence")
+
+# write a tebale of 5 in pyathon 
+print("5*1=5")
+print("5*2=10")
+print("5*3=15")
+print("5*4=20")
+print("5*5=25")
+print("5*6=30")
+print("5*7=35")
+print("5*8=40")
+print("5*9=45")
+print("5*10=50")
+
+# write a program to print the content of a directory using os module 
+
+print(os.listdir())
+
+# install an extrnal module in python 
+import sklearn 
+
+print("hello world")
+
+# lable the program written in 4 with comment
+w=6
+
+print("this is a label")
