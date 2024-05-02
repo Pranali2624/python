3d07362c776f2277398ddb888f635c5ebd41dc50
Pranali Waghmare <126958148+Pranali2624@users.noreply.github.com>
2024-05-02 15:40:27 +0530

    Create Exercise1.py

---
+++ Exercise1.py
@@ -0,0 +1,9 @@
+# create a calculater capable of performing additon subtarction multiplecation and division operation on two numbers.
+a = 22
+b = 12
+print("The value of ", a, "+", 12, "is:", a + b)
+print("The value of ", a, "*", 12, "is:", a * b)
+print("The value of ", a, "-", 12, "is:", a - b)
+print("The value of ", a, "/", 12, "is:", a / b)
+print("The value of ", a, "//", 12, "is:", a // b)
+print("The value of ", a, "%", 12, "is:", a % b)
