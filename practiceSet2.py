71044ad9a684dfc52dbc2b6c08855a79d9cbd172
Pranali Waghmare <126958148+Pranali2624@users.noreply.github.com>
2024-05-02 16:35:21 +0530

    Create practiceSet2.py

---
+++ practiceSet2.py
@@ -0,0 +1,28 @@
+# write a python program to add two numbers
+a = int(input("Enter a number: "))
+b = int(input("Enter a number: "))
+print(a+b)
+
+#write a program to find  reminder when a number is divided by 2
+a = int(input("Enter a number: "))
+print(a%2)
+
+
+# check the type of the variable assigned using input() function 
+a = int(input("Enter a number: "))
+print(type(a))
+
+# use comperison operters to find out whether a given variable a is gether then 'b' or not take a=34 and b=80
+a=34
+b=80
+print(a>b)
+
+# wirte a python program to find average of two numbers entered by the user
+a = int(input("Enter a number: "))
+b = int(input("Enter a number: "))
+print((a+b)/2)
+
+# write a python program to calculate square of a number entered by the user
+a = int(input("Enter a number: "))
+print(a*a)
+
