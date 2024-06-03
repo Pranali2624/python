716de6f4f69f5f33b53b20303e034acfdce2b9d1
Pranali Waghmare <126958148+Pranali2624@users.noreply.github.com>
2024-06-03 21:49:33 +0530

    practiceSet8.py

--- practiceSet8.py
+++ practiceSet8.py
@@ -1,6 +1,6 @@
 # Write a program to create a dictionary of Hindi words with values as their english translation 
 
-hindi_to_english = {'बिली':'cat ','कुता':'dog','पंछि':'bird'}
+hindi_to_english = {'बिली':'cat ','कुता':'dog','पंछि':'bird','खुर्ची ':'chair' }
 print(hindi_to_english.items())
 
 # Write a program to input eight numbers from the user and display all the uaique numbers (ones)
